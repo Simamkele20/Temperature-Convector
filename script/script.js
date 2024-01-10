d3155f6ee86da26d626c4081083b8d8f9e529948
Simamkele20 <Princetancu06@gmail.com>
2024-01-10 09:16:58 +0200

    Updates

--- script/script.js
+++ script/script.js
@@ -10,9 +10,7 @@ document.getElementById("celsius").onchange = function() {
 document.getElementById("fahrenheit").onchange = function() {
   lastEdited = "fahrenheit";
 };
-document.getElementById("kelvin").onchange = function() {
-  lastEdited = "kelvin";
-};
+
 
 function convert(temp) {
 
@@ -23,18 +21,17 @@ function convert(temp) {
   var fahrenheit = document.getElementById("fahrenheit").value;
   fahrenheit = parseFloat(fahrenheit);
 
-  var kelvin = document.getElementById("kelvin").value;
-  kelvin = parseFloat(kelvin);
+
 
 
   var conversionC;
   var conversionF;
-  var conversionK;
+
 
 
   if (lastEdited === "celsius") {
     conversionF = (celsius * 9 / 5) + 32;
-    conversionK = celsius + 273;
+
    
     document.getElementById("fahrenheit").value = Math.round(conversionF);
     document.getElementById("kelvin").value = Math.round(conversionK);
@@ -42,15 +39,11 @@ function convert(temp) {
     
     else if (lastEdited === "fahrenheit") {
     conversionC = (fahrenheit - 32) * 5 / 9;
-    conversionK = conversionC + 273;
+
    
     document.getElementById("celsius").value = Math.round(conversionC);
-    document.getElementById("kelvin").value = Math.round(conversionK);
-  } 
-  
-    else if (lastEdited === "kelvin") {
-    conversionC = kelvin - 273;
-    conversionF = (conversionC * 9 / 5) + 32;
+    
+
 
     document.getElementById("celsius").value = Math.round(conversionC);
     document.getElementById("fahrenheit").value = Math.round(conversionF);
@@ -59,12 +52,12 @@ function convert(temp) {
   
   document.getElementById("celsius").innerHTML = conversionC;
   document.getElementById("fahrenheit").innerHTML = conversionF;
-  document.getElementById("kelvin").innerHTML = conversionK;
+
 }
 
 
 function reset() {
   document.getElementById("celsius").value = 0;
   document.getElementById("fahrenheit").value = 0;
-  document.getElementById("kelvin").value = 0;
+
 }
